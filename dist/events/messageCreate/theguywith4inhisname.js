02428d69d227ff0b3a53bf2cc5eca54376925f4a
Marco Antolini <marco.antolini2001@gmail.com>
2024-02-07 12:32:22 +0100

    Delete dist directory

--- dist/events/messageCreate/theguywith4inhisname.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(message, client, handler) {
-    if (message.content.toLowerCase().includes("the guy with 4 in his name")) {
-        message.reply("Seems like you're talking about me!");
-    }
-}
-exports.default = default_1;
